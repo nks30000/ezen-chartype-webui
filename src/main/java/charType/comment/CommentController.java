4d9ed5ceb29c195edfbb5343d43aa8dafd27a466
kwon <kwon@DESKTOP-85ICEG9>
2021-10-21 21:04:59 +0900

    comment 작성 및 변경 (10.21)

--- src/main/java/charType/comment/CommentController.java
+++ src/main/java/charType/comment/CommentController.java
@@ -1,5 +1,44 @@
 package charType.comment;
 
-public class CommentController {
+import charType.utils.common.mapper.CommandMap;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping(value="/community/timeline")
+public class CommentController {
+	
+	@RequestMapping(value="/commentWrite")
+	public ModelAndView writeComment(CommandMap commandMap)
+		throws Exception {
+		ModelAndView mv = new ModelAndView();
+		return mv;
+	}
+	
+	@RequestMapping(value="/commentSelect")
+	public ModelAndView selectComment(CommandMap commandMap)
+		throws Exception {
+		ModelAndView mv = new ModelAndView();
+		return mv;
+	}
+	
+	@RequestMapping(value="/commentDelete")
+	public ModelAndView deleteComment(CommandMap commandMap)
+		throws Exception {
+		ModelAndView mv = new ModelAndView();
+		return mv;
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/charType/comment/CommentDAO.java
+++ src/main/java/charType/comment/CommentDAO.java
@@ -1,5 +1,36 @@
 package charType.comment;
 
-public class CommentDAO {
+import java.util.Map;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import charType.utils.common.dao.AbstractDAO;
+
+@Repository("commentDAO")
+public class CommentDAO extends AbstractDAO{
+	
+	private final String NAMESPACE = "comment.";
+	
+	//댓글 입력
+	public void writeComment(Map<String,Object> map) throws Exception {
+		
+		insert("comment.writeComment", map );
+	}
+	
+	//댓글 리스트 가져오기
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectComment(Map<String, Object> map)
+		throws Exception {
+		
+		return (List<Map<String,Object>>) selectList("comment.selectComment", map);
+	}
+	
+	//댓글 삭제하기
+	public void deleteComment(Map<String, Object> map) throws Exception {
+		
+		delete("comment.deleteComment", map);
+	}
+
 
 }
