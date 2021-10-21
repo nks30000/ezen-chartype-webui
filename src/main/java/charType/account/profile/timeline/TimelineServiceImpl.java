d57ece89e45488469d8b5e248ddad38895695ed0
Tae <Tae@192.168.219.154>
2021-10-21 20:54:01 +0900

    Timeline 작성 10.21 -이태환

--- src/main/java/charType/account/profile/timeline/TimelineServiceImpl.java
+++ src/main/java/charType/account/profile/timeline/TimelineServiceImpl.java
@@ -1,5 +1,27 @@
 package charType.account.profile.timeline;
 
-public class TimelineServiceImpl {
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service; 
+
+
+@Service("timelineService")
+public class TimelineServiceImpl implements TimelineService{
+	
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="timelineDAO")
+	private TimelineDAO timelineDAO;
+
+	@Override
+	public List<Map<String, Object>> selectAccountTimeline(Map<String, Object> map) throws Exception {
+		
+		return timelineDAO.selectAccountTimeline(map);
+	}
+	
 
 }
