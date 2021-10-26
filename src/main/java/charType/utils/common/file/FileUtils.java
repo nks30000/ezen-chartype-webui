cf5a15f0cc140dd10ab5b27bf76e2e716cc91ab1
Tae <Tae@192.168.219.154>
2021-10-26 19:35:19 +0900

    파일 업로드 유틸스 -이태환

---
+++ src/main/java/charType/utils/common/file/FileUtils.java
@@ -0,0 +1,65 @@
+package charType.utils.common.file;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+@Component("fileUtils")
+public class FileUtils {
+	private static final String filePath = "C:\\Users\\Tae\\Desktop\\프로젝트\\img\\saved\\";
+	
+	public List<Map<String,Object>> parseInsertFileInfo(Map<String,Object> map, HttpServletRequest request) throws Exception{
+		MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest)request;
+    	Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+    	
+    	MultipartFile multipartFile = null;
+    	String originalFileName = null;
+    	String originalFileExtension = null;
+    	String storedFileName = null;
+    	
+    	List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        Map<String, Object> listMap = null; 
+        
+        String board_num = (String)map.get("BOARD_NUM");
+        
+        File file = new File(filePath);
+        if(file.exists() == false){
+        	file.mkdirs();
+        }
+        
+        while(iterator.hasNext()){
+        	multipartFile = multipartHttpServletRequest.getFile(iterator.next());
+        	if(multipartFile.isEmpty() == false){
+        		originalFileName = multipartFile.getOriginalFilename();
+        		originalFileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        		storedFileName = CommonUtils.getRandomString() + originalFileExtension;
+        		
+        		file = new File(filePath + storedFileName);
+        		multipartFile.transferTo(file);
+        		
+        		listMap = new HashMap<String,Object>();
+        		listMap.put("BOARD_NUM", board_num);
+        		listMap.put("ORIGINAL_FILE_NAME", originalFileName);
+        		listMap.put("SAVED_NM", storedFileName);
+        		listMap.put("FILE_SIZE", multipartFile.getSize());
+        		list.add(listMap);
+        	}
+        }
+		return list;
+	}
+}
+
+
+
+	
+	
+
