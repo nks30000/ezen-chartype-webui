be56c5975566f928f116170d0b47361bca76ec8a
nks30000 <nks30000@gmail.com>
2021-10-28 20:42:47 +0900

    공통 서비스 데이터

---
+++ src/main/java/charType/cons/charConst.java
@@ -0,0 +1,10 @@
+package charType.cons;
+
+public interface charConst {
+
+	/**
+	 * default image upload path
+	 */
+	String UPLOAD_PATH_IMAGE = "C:\\charType\\";
+	
+}
---
+++ src/main/java/charType/cons/listMbti.java
@@ -0,0 +1,28 @@
+package charType.cons;
+
+import java.sql.Array;
+
+public interface listMbti {
+	/*
+	 *  mbti list 
+	 *  
+	 *  */
+	String[] MBTI = {
+		"ISTJ",
+		"ISFJ",
+		"INFJ",
+		"INTJ",
+		"ISTP",
+		"ISFP",
+		"INFP",
+		"INTP",
+		"ESTP",
+		"ESFP",
+		"ENFP",
+		"ENTP",
+		"ESTJ",
+		"ESFJ",
+		"ENFJ",
+		"ENTJ"
+	} ;
+}
