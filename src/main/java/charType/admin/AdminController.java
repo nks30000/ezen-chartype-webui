9c3b198deb41176cdf45c8698e4008c30ed920be
luvoa <luvoa@Aubrey>
2021-11-09 20:20:57 +0900

    admin 회원정보 리스트 완료

--- src/main/java/charType/admin/AdminController.java
+++ src/main/java/charType/admin/AdminController.java
@@ -1,5 +1,48 @@
 package charType.admin;
 
+import java.util.HashMap;
+import java.util.List; 
+import java.util.Map; 
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger; 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import charType.admin.AdminService;
+import charType.account.follow.FollowService;
+import charType.admin.AdminDAO;
+import charType.utils.common.mapper.CommandMap;
+
+@Controller
+@RequestMapping(value="/admin/member")
 public class AdminController {
+	
+	@Resource(name="adminService") 
+	private AdminService adminService; 
+	
+	@RequestMapping(value="/memberlist") 
+	public ModelAndView adminMemberList(CommandMap commandMap, HttpServletRequest request) throws Exception{
+		ModelAndView mv = new ModelAndView("/admin/member/admin_member_list");
+		
+		HttpSession session = request.getSession();
+		String userId = (String) session.getAttribute("session_mem_id");
+		
+		
+		// 유저 리스트 가져오기
+		List<Map<String, Object>> allUserList = adminService.alluserData(commandMap.getMap());
+		commandMap.put("allUserList", allUserList);
+		
+		mv.addObject("map", commandMap.getMap());
+		return mv;
+		
+		
+}
 
 }
--- src/main/java/charType/admin/AdminDAO.java
+++ src/main/java/charType/admin/AdminDAO.java
@@ -1,5 +1,20 @@
 package charType.admin;
 
-public class AdminDAO {
+import java.util.Map;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import charType.utils.common.dao.AbstractDAO;
+
+@Repository("adminDao")
+public class AdminDAO extends AbstractDAO {
+	
+	//모든 유저의 가입 정보 가져오기
+		@SuppressWarnings("unchecked")
+		public List<Map<String, Object>> alluserData(Map<String, Object> map)
+			throws Exception {
+			return (List<Map<String,Object>>) selectList("admin.alluserData", map);
+		}
 
 }
